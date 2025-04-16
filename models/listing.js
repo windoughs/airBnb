9f4d6501ef9d8221364683e5df785babe5480b1d
Anjali Dixit <122504032+windoughs@users.noreply.github.com>
2025-04-16 19:27:06 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -25,17 +25,17 @@ const listingSchema = new Schema({
             type:Schema.Types.ObjectId,
             ref:"User"
     },
-    geometry: {
-        type: {
-          type: String, // Don't do `{ location: { type: String } }`
-          enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
-        },
-        coordinates: {
-          type: [Number],
-          required: true
-        }
-      }
+    // geometry: {
+    //     type: {
+    //       type: String, // Don't do `{ location: { type: String } }`
+    //       enum: ['Point'], // 'location.type' must be 'Point'
+    //       required: true
+    //     },
+    //     coordinates: {
+    //       type: [Number],
+    //       required: true
+    //     }
+    //   }
 })
 //middlewares
 listingSchema.post("findOneAndDelete",async(listing)=>{
