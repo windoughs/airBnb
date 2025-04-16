6d69a6f48a971bed982aa592b7bffd0354ab1e76
Anjali Dixit <122504032+windoughs@users.noreply.github.com>
2025-04-16 19:35:10 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,15 @@
-mapboxgl.accessToken = mapToken;
-  const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center:listing.geometry.coordinates, // starting position [lng, lat]
-  zoom: 9, // starting zoom
-  });
-// Create a default Marker and add it to the map.
-const marker = new mapboxgl.Marker({color:"red"})
-.setLngLat(listing.geometry.coordinates)
-.setPopup(
-  new mapboxgl.Popup({offset: 25}).setHTML(`<h3>Here, is your destination:</h3><p>${listing.location}</p>`)
-)
-.addTo(map);
+// mapboxgl.accessToken = mapToken;
+//   const map = new mapboxgl.Map({
+//   container: 'map', // container ID
+//   style: 'mapbox://styles/mapbox/streets-v12',
+//   center:listing.geometry.coordinates, // starting position [lng, lat]
+//   zoom: 9, // starting zoom
+//   });
+// // Create a default Marker and add it to the map.
+// const marker = new mapboxgl.Marker({color:"red"})
+// .setLngLat(listing.geometry.coordinates)
+// .setPopup(
+//   new mapboxgl.Popup({offset: 25}).setHTML(`<h3>Here, is your destination:</h3><p>${listing.location}</p>`)
+// )
+// .addTo(map);
 
